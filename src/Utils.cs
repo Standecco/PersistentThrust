fd77dfcfeabcaa13b4efa05ff57d46fa8e3cdd9b
Standecco <dlombardo32@gmail.com>
2020-02-16 11:12:56 +0100

    Reformat *all* files

--- src/Utils.cs
+++ src/Utils.cs
@@ -2,19 +2,27 @@ using System;
 using System.Linq;
 using UnityEngine;
 
-namespace PersistentThrust {
+namespace PersistentThrust
+{
 
-    public class Utils {
+	public class Utils
+	{
 
-	// Format thrust into mN, N, kN
-	public static string FormatThrust(double thrust) {
-	    if (thrust < 0.001) {
-		return Math.Round(thrust * 1000000.0, 3).ToString() + " mN";
-	    } else if (thrust < 1.0) {
-		return Math.Round(thrust * 1000.0, 3).ToString() + " N";
-	    } else {
-		return Math.Round(thrust, 3).ToString() + " kN";
-	    }
+		// Format thrust into mN, N, kN
+		public static string FormatThrust(double thrust)
+		{
+			if(thrust < 0.001)
+			{
+				return Math.Round(thrust * 1000000.0, 3).ToString() + " mN";
+			}
+			else if(thrust < 1.0)
+			{
+				return Math.Round(thrust * 1000.0, 3).ToString() + " N";
+			}
+			else
+			{
+				return Math.Round(thrust, 3).ToString() + " kN";
+			}
+		}
 	}
-    }
 }
